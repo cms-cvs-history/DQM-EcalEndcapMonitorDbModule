4c500f919d2dcf0c213a610245625d1290053c20
cmsbuild <>
2007-04-02 16:20:49 +0000

    This commit was manufactured by cvs2git to create tag 'DQM- EcalEndcapMonitorDbModule-old-plugin-manager'.
    Sprout from master 2007-04-02 16:20:48 UTC Giuseppe Della Ricca <sha1-2ac9df825d52a0270fbcfdafbd6206ce1b324729@cern.ch> ''
    Delete:
        doc/EcalEndcapMonitorDbModule.doc
        doc/html/index.html
        doc/html/overview.html
        interface/EcalEndcapMonitorDbModule.h
        interface/MonitorElementsDb.h
        interface/MonitorXMLParser.h
        src/EcalEndcapMonitorDbModule.cc
        src/MonitorElementsDb.cc
        src/MonitorXMLParser.cc
        src/SealModule.cc

--- src/SealModule.cc
+++
@@ -1,9 +0,0 @@
-
-#include "PluginManager/ModuleDef.h"
-
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include <DQM/EcalEndcapMonitorDbModule/interface/EcalEndcapMonitorDbModule.h>
-
-DEFINE_FWK_MODULE(EcalEndcapMonitorDbModule);
-
